a316e5dd4cf09954ff834444896d5198d2e98724
León Yannik López Rojas <leonrlc@hotmail.com>
2020-01-21 16:53:51 -0600

    Added a new rout

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,10 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("leon") { req in
+        return "Saludos, mira, parece que esto está funcionando:   :)   :D"
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
