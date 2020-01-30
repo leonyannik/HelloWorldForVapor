07890efe7942715a5f56fd964137b294550a8688
León Yannik López Rojas <leonrlc@hotmail.com>
2020-01-30 12:33:11 -0600

    New Route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -19,6 +19,10 @@ public func routes(_ router: Router) throws {
     router.get("juanPablo") { req in
         return "Hola Juan Pablo!!!!!\n\nNo inventes man, me costó 4 días no manches\n pero mira ya pude.\n\n\nCorre con el Nginx para que pueda tener el fregado candadito... Y la app de Vapor corre consupervisor para que si muere o algo el servidor, luego luego que arranque, comience la app. :D\n ahora supongo que debo empezar a hacer como un blog o algo lol."
     }
+    
+    router.get("david") { req in
+        return "Hola David!!!!!\n\nMe costó un poco de trabajo pero fue divertido\n pero mira ya pude.\n\n\nEl framework se llama Vapor, y corre en un servidor tipo Nginx que es similar al Apache. Y la app corre desde algo que se llama Supervisor para que corra aunque la máquina se reinicie. \nEl hosting es en DigitalOcean y es en Ubuntu. \nLo que más trabajo me costó fué ponerle el candadito con el sertificado SSL porque casi no encontraba documentación."
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
